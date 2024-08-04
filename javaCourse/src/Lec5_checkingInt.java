71272881826605193cd6696d23fd8e62ef32774d
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-04 19:30:55 +0530

    User Input , checking int and string

---
+++ javaCourse/src/Lec5_checkingInt.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Lec5_checkingInt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean b1 = sc.hasNextInt();
+        System.out.println(b1);
+
+//        String str = sc.next();
+//        String str2 = sc.nextLine();
+//        System.out.println(str);
+//
+
+    }
+}
