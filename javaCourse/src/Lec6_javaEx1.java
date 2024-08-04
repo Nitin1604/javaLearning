88b7995b0e1eb3edd08de63a251f463a0e9e4583
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-04 22:29:03 +0530

    Calculating the CBSE percentage

--- javaCourse/src/Lec6_javaEx1.java
+++ javaCourse/src/Lec6_javaEx1.java
@@ -1,4 +1,4 @@
-    /*
+    /* 
     Problem Statement:-
       Write a program to calculate the percentage of a given student in the CBSE board exam.
       His marks from 5 subjects must be taken as input from the keyboard. (Marks are out of 100)
