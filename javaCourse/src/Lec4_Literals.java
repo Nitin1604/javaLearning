4e7e3fabdd3eaef57fda1f319c79eb78dcf14064
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-04 18:44:05 +0530

    Java Learning

---
+++ javaCourse/src/Lec4_Literals.java
@@ -0,0 +1,25 @@
+public class Lec4_Literals {
+    public static void main(String[] args) {
+        byte age = 26;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        boolean a = true;
+
+        System.out.println(age); // Output : 26
+        System.out.println(age2); // Output : 56
+        System.out.println(age3); // Output : 87
+        System.out.println(ageDino); // Output : 5666666666666
+        System.out.println(ch); // Output : A
+        System.out.println(f1); // Output : 5.6
+        System.out.println(d1); // Output : 4.66
+        System.out.println(a); // Output : true
+
+        String str = "Nitin";
+        System.out.println(str); // Output : Nitin
+
+    }
+}
---
+++ javaCourse/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+       // Quick Quiz: Write a Java program to add three numbers,
+        // Quick Quiz is mention in this Tutorial => Java Tutorial: Variables and Data Types in Java Programming
+        System.out.print("The sum of three numbers are: ");
+        int num1 = 21;
+        int num2 = 16;
+        int num3 = 20;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
