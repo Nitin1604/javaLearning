68f1672109e660c91e3af6de54f9c297efe51645
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-05 13:30:29 +0530

    Chapter 9 Quick Quiz

---
+++ javaCourse/src/Lec9_quickQuiz.java
@@ -0,0 +1,15 @@
+public class Lec9_quickQuiz {
+    public static void main(String[] args) {
+        // Quick Quiz
+        int x = 6;
+        int y = 1;
+        int xy = x * y/2;
+        System.out.println(xy);
+
+        int b = 10;
+        int c = 20;
+        int a = 10;
+        int bca = b*b - (4*a*c)/(2*a);
+        System.out.println(bca);
+    }
+}
