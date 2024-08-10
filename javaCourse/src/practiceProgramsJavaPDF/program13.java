89fefc60a55c91a653e614571a0df78357f2e05e
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-10 23:36:49 +0530

    Practice Problems from Java 11th Edition Book

---
+++ javaCourse/src/practiceProgramsJavaPDF/program13.java
@@ -0,0 +1,10 @@
+package practiceProgramsJavaPDF;
+
+public class program13 {
+    // One-Dimensional Arrays's example 2
+    public static void main(String[] args) {
+            //      index  0    1  2    3  4   5   6   7   8   9   10  11
+        int monthDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("March has " + monthDays[2] + " days.");
+    }
+}
