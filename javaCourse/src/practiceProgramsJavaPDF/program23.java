4c26b38ae9b9af28f30f10d17b6d420008939dda
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-11 11:21:10 +0530

    Practice Problems from Java 11th Edition Book

---
+++ javaCourse/src/practiceProgramsJavaPDF/program23.java
@@ -0,0 +1,19 @@
+package practiceProgramsJavaPDF;
+
+public class program23 {
+    // Increment and Decrement based examples
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c;
+        int d;
+        c = ++b; // 3
+        d = a++; // 1
+        c++; // 4
+        System.out.println("a is: " + a); // 2
+        System.out.println("b is: " + b); // 3
+        System.out.println("c is: " + c); // 4
+        System.out.println("d is: " + d); // 1
+
+    }
+}
