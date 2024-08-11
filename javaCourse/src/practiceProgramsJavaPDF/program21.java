aea3d8a5f3906a53367eeb635432fa9681e9fba3
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-11 11:20:14 +0530

    Practice Problems from Java 11th Edition Book

---
+++ javaCourse/src/practiceProgramsJavaPDF/program21.java
@@ -0,0 +1,12 @@
+package practiceProgramsJavaPDF;
+
+public class program21 {
+    // The Modulus Operator's examples
+    public static void main(String[] args) {
+        int x =  11;
+        double y = 17;
+
+        System.out.println("x mod 10: " + x % 10);
+        System.out.println("y mod 10: " + y % 10);
+    }
+}
