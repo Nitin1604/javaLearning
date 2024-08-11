a228dd7a103b947e83e5ab51e3201e1a500637e0
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-11 11:19:37 +0530

    Practice Problems from Java 11th Edition Book

---
+++ javaCourse/src/practiceProgramsJavaPDF/program20.java
@@ -0,0 +1,32 @@
+package practiceProgramsJavaPDF;
+
+public class program20 {
+     // The Basic Arithmetic Operators's examples
+     public static void main(String[] args) {
+         // Arithmetic using integers
+         System.out.println("Integers Arithmetic:-");
+            int a = 1 + 1; // 2
+            int b = a * 3; // 6
+            int c = b / 4; // 1
+            int d = c - a; // -1
+            int e = -d; // 1
+         System.out.println("a is: " + a); // a is: 2
+         System.out.println("b is: " + b); // b is: 6
+         System.out.println("c is: " + c); // c is: 1
+         System.out.println("d is: " + d); // d is: -1
+         System.out.println("e is: " + e); // e is: 1
+
+         // Arithmetic using doubles
+         System.out.println("\nFloating points Arithmetic");
+         double da = 1 + 1;  // 2.0
+         double db = da * 3; // 6.0
+         double dc = db / 4; // 1.5
+         double dd = dc - a; // -0.5
+         double de = - dd; // 0.5
+         System.out.println("da is: " + da); // da is: 2.0
+         System.out.println("db is: " + db); // db is: 6.0
+         System.out.println("dc is: " + dc); // dc is: 1.5
+         System.out.println("dd is: " + dd); // dd is: -0.5
+         System.out.println("de is: " + de); // de is: 0.5
+     }
+}
