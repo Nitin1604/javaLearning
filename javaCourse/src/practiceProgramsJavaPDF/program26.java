a39d1ae42c106242421bc1962e9340ddbe64f847
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-12 19:06:55 +0530

    Practice Problems from Java 11th Edition Book

---
+++ javaCourse/src/practiceProgramsJavaPDF/program26.java
@@ -0,0 +1,13 @@
+package practiceProgramsJavaPDF;
+
+public class program26 {
+    //  Ternary Operator
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 108;
+        k =  i < 0 ? -i : i; // get absolute value of i
+        System.out.println("Absolute value of " + i + " is " + k);
+
+    }
+}
