9d3140295b39dda55f73e180e6a9d3c953beee2d
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-08-12 19:06:22 +0530

    Practice Problems from Java 11th Edition Book

---
+++ javaCourse/src/practiceProgramsJavaPDF/program25.java
@@ -0,0 +1,14 @@
+package practiceProgramsJavaPDF;
+
+public class program25 {
+    // Left Shift Example
+    public static void main(String[] args) {
+    byte a = 64, b;
+    int i;
+    i = a << 2;
+    b = (byte)(a << 2);
+        System.out.println("Original value of a is: " + a);
+        System.out.println("i and b are: " + i + " " + b);
+
+    }
+}
