a752fb2814e9882599ac1b2398f35141ef4cd348
Nitin <nitinsokhal@gmail.com>
2024-08-31 10:14:57 +0530

    Practice Problems from Java 11th Edition Book

---
+++ javaCourse/src/practiceProgramsJavaPDF/program42.java
@@ -0,0 +1,19 @@
+package practiceProgramsJavaPDF;
+
+class program42 {
+    static int a =  3;
+    static int b;
+
+static void meth(int x){
+    System.out.println("x is: " + x + " ," + " a is: " + a + " ," + " and b is: " + b );
+  }
+static {
+    System.out.println("Static block initialized");
+    b = a * 4;
+ }
+    public static void main(String[] args) {
+        meth(42);
+    }
+}
+
+
