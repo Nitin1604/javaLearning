2083bae7b8a164e15523886665d47e970796954b
Nitin <nitinsokhal@gmail.com>
2024-09-02 08:11:09 +0530

    Practice Problems from Java 11th Edition Book

---
+++ javaCourse/src/practiceProgramsJavaPDF/program43.java
@@ -0,0 +1,12 @@
+package practiceProgramsJavaPDF;
+
+// This program demonstrates the length  array member.
+public class program43 {
+    public static void main(String[] args) {
+        int arr1[] = new int[10];
+        int arr2[] = {3, 5, 7, 1, 8, 99, 44, -10};
+        int arr3[] = {4, 3, 2, 1};
+
+        System.out.println("Length of arr1 is: " +arr1.length + " , length of arr2 is: " + arr2.length + " and length of arr3 is: " + arr3.length);
+    }
+}
