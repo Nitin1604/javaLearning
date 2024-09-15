2742ee8a7ed8f9d9b8260475c3194a83a81aa48c
Nitin <nitinsokhal@gmail.com>
2024-09-15 11:54:35 +0530

    My Own Practice for Java

--- javaCourse/src/interview/practiceQues/characterCount.java
+++ javaCourse/src/interview/practiceQues/characterCount.java
@@ -2,30 +2,39 @@ package interview.practiceQues;
 
 import java.util.Scanner;
 
-
 public class characterCount {
 
-   public static void charCount(String stringInput){
-        for (int i = 0; i<stringInput.length();i++){
-            char charEval = stringInput.charAt(i);
-            int count = 0;
+    public static void charCount(String stringInput) {
+        boolean[] counted = new boolean[stringInput.length()];
+        int repeatCount = 0;
 
-            for (int j = 0; j <stringInput.length();j++){
-                if (stringInput.charAt(j)== charEval){
-                    count++;
+        for (int i = 0; i < stringInput.length(); i++) {
+            if (!counted[i]) {
+                char charEval = stringInput.charAt(i);
+                int count = 0;
+
+                for (int j = 0; j < stringInput.length(); j++) {
+                    if (stringInput.charAt(j) == charEval) {
+                        count++;
+                        counted[j] = true;
+                    }
+                }
+
+                if (count > 1) {
+                    System.out.println(charEval + " is coming " + count + " times.");
+                    repeatCount++;
                 }
             }
-            if (count >= 0){
-                System.out.println(charEval + " is coming " + count + " times.");
-            }
         }
-   }
+
+        System.out.println("Count is: " + repeatCount);
+    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string: ");
-        String strInput  = sc.next();
+        String strInput = sc.nextLine();
         charCount(strInput);
     }
 
