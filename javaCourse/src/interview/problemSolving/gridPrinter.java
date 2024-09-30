163b653eba989c383e806de608d47153f859697a
Nitin <nitinsokhal@gmail.com>
2024-09-30 18:05:44 +0530

    Another practice for Java

--- javaCourse/src/interview/problemSolving/gridPrinter.java
+++ javaCourse/src/interview/problemSolving/gridPrinter.java
@@ -1,9 +1,10 @@
 package interview.problemSolving;
 
+// Ques 4. Write a Java program to calculate the average value of array elements.
 public class gridPrinter {
     public static void main(String[] args) {
         int numInput = 10;
-        for (int innerCont = 0; innerCont < 10; innerCont++){
+        for (int innerCont = 0; innerCont < 5; innerCont++){
             System.out.println();
             for(int i = 0; i<=numInput;i++){
                 System.out.print("-" + " ");
